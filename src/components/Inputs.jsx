8c2b45aa632e3de3b2d4b7071d666fa4086123c4
Sandro Mendoza <sandro.mendoza@ffufm.com>
2025-05-09 18:51:37 +0800

    fixed input to removed 0

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -16,14 +16,21 @@ export default function Inputs({ variable, func }) {
         inputMode="numeric"
         pattern="[0-9]*"
         className="appearance-none w-full md:w-50 lg:w-40 border border-gray-400 rounded px-2 py-1 focus:outline-none text-center"
-        value={variable}
+        value={variable === 0 ? "" : variable}
         onChange={(e) => {
-          const value = parseInt(e.target.value);
-          if (!isNaN(value)) {
-            func(value);
+          const val = e.target.value;
+
+          if (val === "") {
+            func(0);
+          } else {
+            const num = parseInt(val, 10);
+            if (!isNaN(num)) {
+              func(num);
+            }
           }
         }}
       />
+
       <button
         className="w-16 md:w-12 lg:w-16 h-8 flex items-center justify-center border rounded text-xl bg-black text-white cursor-pointers"
         onClick={() => func(variable + 1)}
